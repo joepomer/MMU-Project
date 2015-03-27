a8198e3fdc8a23be8cbe37dfa4f5ed985e23101b
joepomer <joepomer@hotmail.com>
2015-03-27 12:48:20 +0000

    changed

--- WindowsApplication3(latest)/WindowsApplication3/Start.vb
+++ WindowsApplication3(latest)/WindowsApplication3/Start.vb
@@ -7,7 +7,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         Me.Hide()
-        Rule_choice.Show()
+        Creation.Show()
 
     End Sub
 End Class
